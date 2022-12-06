ba8345f63cea8ffb256290c49133010c4ae1a62c
Heba Hiari <hebahiary+github@gmail.com>
2022-12-06 14:00:14 -0800

    Initial commit

---
+++ client/src/Components/Footer/Footer.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./footer.scss";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Children</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            tempus, turpis et dictum bibendum, magna magna accumsan eros,
+            hendrerit auctor lectus lectus id nulla. Fusce vitae nisi mi.
+            Vestibulum et consequat sem. Nullam varius tempor sapien vel
+            vestibulum.
+          </span>
+        </div>
+        <div className="item">
+          <h1>Contact</h1>
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris
+            tempus, turpis et dictum bibendum, magna magna accumsan eros,
+            hendrerit auctor lectus lectus id nulla. Fusce vitae nisi mi.
+            Vestibulum et consequat sem. Nullam varius tempor sapien vel
+            vestibulum.
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">TechWearDom</span>
+          <span>© 2022 TechWearDom. All rights reserved.</span>
+        </div>
+        <div className="right">
+          <img src="/img/payments.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import "./navbar.scss";
+import {
+  Search,
+  FavoriteBorderOutlined,
+  PersonOutlineOutlined,
+  KeyboardArrowDown,
+  ShoppingCartOutlined,
+} from "@mui/icons-material";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <div className="item">
+            <img src="/img/eng.png" alt="" className="lang" />
+            <KeyboardArrowDown />
+          </div>
+          <div className="item">
+            USD
+            <KeyboardArrowDown />
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/1">
+              Women
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/2">
+              Men
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/3">
+              Children
+            </Link>
+          </div>
+        </div>
+        <div className="center">
+          <Link className="link" to="/">
+            TechWearDom
+          </Link>
+        </div>
+        <div className="right">
+          <div className="item">
+            <Link className="link" to="/">
+              Home
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/1">
+              About
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/1">
+              Contact
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/1">
+              Stores
+            </Link>
+          </div>
+          <div className="icons">
+            <Search />
+            <PersonOutlineOutlined />
+            <FavoriteBorderOutlined />
+            <div className="cartIcon">
+              <ShoppingCartOutlined />
+              <span>0</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
