a0bb34f114f4f9626717aa6f6e088caca5410867
Heba Hiari <hebahiary+github@gmail.com>
2022-12-23 19:52:56 -0800

    updated FeaturedProducts to fetch from strapi

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -2,18 +2,33 @@ import "./card.scss";
 import { Link } from "react-router-dom";
 
 export default function Card({ item }) {
+  console.log({ item });
   return (
-    <Link className="link" to={`/product/${item.id}`}>
+    <Link className="link" to={`/product/${item?.id}`}>
       <div className="card">
         <div className="image">
           {item.isNew && <span>New</span>}
-          <img src={item.images[0]} alt="" className="mainImage" />
-          <img src={item.images[1]} alt="" className="secondImage" />
+          <img
+            src={
+              process.env.REACT_APP_BASE_URL +
+              item?.attributes.images.data[0].attributes.url
+            }
+            alt=""
+            className="mainImage"
+          />
+          <img
+            src={
+              process.env.REACT_APP_BASE_URL +
+              item?.attributes.images.data[1].attributes.url
+            }
+            alt=""
+            className="secondImage"
+          />
         </div>
-        <h2>{item.title}</h2>
+        <h2>{item?.attributes.title}</h2>
         <div className="prices">
-          <h3>${item.oldPrice}</h3>
-          <h3>${item.price}</h3>
+          <h3>${item?.attributes.oldPrice}</h3>
+          <h3>${item?.attributes.price}</h3>
         </div>
       </div>
     </Link>
--- client/src/pages/Home/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/pages/Home/FeaturedProducts/FeaturedProducts.jsx
@@ -1,53 +1,78 @@
+import { useEffect, useState } from "react";
 import Card from "../../../Components/Card/Card";
 import "./featuredProducts.scss";
+import axios from "axios";
 
 export default function FeaturedProducts({ type }) {
-  const data = [
-    {
-      id: 1,
-      images: [
-        "https://gothbb.com/wp-content/uploads/2022/04/Cool-Girl-Locomotive-Short-Top_1-600x600.jpg",
-        "https://gothbb.com/wp-content/uploads/2022/04/Cool-Girl-Locomotive-Short-Top_3-600x600.jpg",
-      ],
-      title: "Mesh Belted Top",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 1,
-      images: [
-        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTlEG0IWG8Hz1s34-LKs4FgWFIuwDyd-k5nF2qww0gWygubAJ5xFUTWYK2mM9eynrc9J0VN5u46Wy4N5bTrhOlelVFlr1hx&usqp=CAY",
-        "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRJFk5k-qUAynYQLRZGWYIKObVBEwnGyrUMu2QPdTg3gAOArJsK09y2ksiaKqb2UN0VEZoVaDAB5N8fbif5XFLMejjHYKoLUQ&usqp=CAY",
-      ],
-      title: "Baggy Chain Pants",
-      isNew: true,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 1,
-      images: [
-        "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTyd1M479KdAhqFrIUYgoslGRmXXIlY9wXi9ByqpyDXiJuAk_vT52SaWlbdByW8fiW-ZDg6IlsZ7dG7nuBeZL8GnGcvdQC3rg&usqp=CAY",
-        "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTSSWjvnTxbSk9FhvMtLhFR3NISPoT6o3KxRsP7LkA7SCRXz06gkxTpN1EpXUqPD6wOqIWiOOVBgiLZ543Z4p_xC9U4_0cw&usqp=CAY",
-      ],
-      title: "Baggy Strap Pants",
-      isNew: false,
-      oldPrice: 19,
-      price: 12,
-    },
-    {
-      id: 1,
-      images: [
-        "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRl29KUsOt32V7MJIZLpZDVeZJV0LHqs7nY66bT0a57EsW8RmM3Xl4wUbYjBTtkdquIVEGOLII_Q-4ohDFgv7kvpKgyv6p7LJbPmRXWcC8Pgh5F4yHO-YUviw&usqp=CAE",
-        "",
-      ],
-      title: "Acrylic Glasses",
-      isNew: false,
-      oldPrice: 19,
-      price: 12,
-    },
-  ];
+  // const data = [
+  //   {
+  //     id: 1,
+  //     images: [
+  //       "https://gothbb.com/wp-content/uploads/2022/04/Cool-Girl-Locomotive-Short-Top_1-600x600.jpg",
+  //       "https://gothbb.com/wp-content/uploads/2022/04/Cool-Girl-Locomotive-Short-Top_3-600x600.jpg",
+  //     ],
+  //     title: "Mesh Belted Top",
+  //     isNew: true,
+  //     oldPrice: 19,
+  //     price: 12,
+  //   },
+  //   {
+  //     id: 1,
+  //     images: [
+  //       "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTlEG0IWG8Hz1s34-LKs4FgWFIuwDyd-k5nF2qww0gWygubAJ5xFUTWYK2mM9eynrc9J0VN5u46Wy4N5bTrhOlelVFlr1hx&usqp=CAY",
+  //       "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRJFk5k-qUAynYQLRZGWYIKObVBEwnGyrUMu2QPdTg3gAOArJsK09y2ksiaKqb2UN0VEZoVaDAB5N8fbif5XFLMejjHYKoLUQ&usqp=CAY",
+  //     ],
+  //     title: "Baggy Chain Pants",
+  //     isNew: true,
+  //     oldPrice: 19,
+  //     price: 12,
+  //   },
+  //   {
+  //     id: 1,
+  //     images: [
+  //       "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTyd1M479KdAhqFrIUYgoslGRmXXIlY9wXi9ByqpyDXiJuAk_vT52SaWlbdByW8fiW-ZDg6IlsZ7dG7nuBeZL8GnGcvdQC3rg&usqp=CAY",
+  //       "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcTSSWjvnTxbSk9FhvMtLhFR3NISPoT6o3KxRsP7LkA7SCRXz06gkxTpN1EpXUqPD6wOqIWiOOVBgiLZ543Z4p_xC9U4_0cw&usqp=CAY",
+  //     ],
+  //     title: "Baggy Strap Pants",
+  //     isNew: false,
+  //     oldPrice: 19,
+  //     price: 12,
+  //   },
+  //   {
+  //     id: 1,
+  //     images: [
+  //       "https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRl29KUsOt32V7MJIZLpZDVeZJV0LHqs7nY66bT0a57EsW8RmM3Xl4wUbYjBTtkdquIVEGOLII_Q-4ohDFgv7kvpKgyv6p7LJbPmRXWcC8Pgh5F4yHO-YUviw&usqp=CAE",
+  //       "",
+  //     ],
+  //     title: "Acrylic Glasses",
+  //     isNew: false,
+  //     oldPrice: 19,
+  //     price: 12,
+  //   },
+  // ];
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log("hello ", process.env);
+        const response = await axios.get(
+          process.env.REACT_APP_API_URL + "/products?populate=*",
+          {
+            headers: {
+              Authorization: "bearer " + process.env.REACT_APP_API_TOKEN,
+            },
+          }
+        );
+        setProducts(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="featuredProducts">
@@ -62,7 +87,7 @@ export default function FeaturedProducts({ type }) {
         </p>
       </div>
       <div className="bottom">
-        {data.map((item) => (
+        {products.map((item) => (
           <Card item={item} key={item.id} />
         ))}
       </div>
