6caae41ee182381f0a4943491c2bae955dc8c2e8
Heba Hiari <hebahiary+github@gmail.com>
2022-12-26 11:51:22 -0800

    added redux-persist
    items now stay in cart when refreshing

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { store } from "./redux/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import Loading from "./Components/Loading/Loading";
+import { persistor, store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={<Loading />} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
