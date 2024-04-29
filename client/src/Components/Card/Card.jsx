ec5810a7f585a47395fe4a15699e19ab9ba261ce
Heba Hiari <45672425+hebahiari@users.noreply.github.com>
2024-04-28 21:40:48 -0700

    updated card styling

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -26,7 +26,7 @@ export default function Card({ item }) {
         </div>
         <h2>{item?.attributes.title}</h2>
         <div className="prices">
-          <h3>${item?.attributes.oldPrice}</h3>
+          {item?.attributes.oldPrice && <h3 className="crossed">${item?.attributes.oldPrice}</h3>}
           <h3>${item?.attributes.price}</h3>
         </div>
       </div>
